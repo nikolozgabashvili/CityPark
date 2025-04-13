1a0269b452b99d452161c9e2e708d362e454e8fe
nikoloz gabashvili <nika2005nika2005@gmail.com>
2025-04-13 17:10:16 +0400

    made some changes to settings

--- app/src/main/java/ge/tbca/city_park/presentation/features/settings/screen/SettingsViewModel.kt
+++ app/src/main/java/ge/tbca/city_park/presentation/features/settings/screen/SettingsViewModel.kt
@@ -2,13 +2,25 @@ package ge.tbca.city_park.presentation.features.settings.screen
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ge.tbca.city_park.domain.core.usecase.GetSavedThemeUseCase
 import ge.tbca.city_park.presentation.core.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() :
-    BaseViewModel<SettingsState, SettingsEffect, SettingsEvent>(SettingsState()) {
+class SettingsViewModel @Inject constructor(
+    private val getSavedThemeUseCase: GetSavedThemeUseCase,
+) : BaseViewModel<SettingsState, SettingsEffect, SettingsEvent>(SettingsState()) {
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            getSavedThemeUseCase().collect { theme ->
+                updateState { copy(currentThemeMode = theme) }
+
+            }
+        }
+    }
 
     override fun onEvent(event: SettingsEvent) {
         when (event) {
