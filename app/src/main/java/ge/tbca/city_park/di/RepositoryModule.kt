797096e74a7e383aeda8fb448ed2c8a60ba80d41
nikoloz gabashvili <nika2005nika2005@gmail.com>
2025-04-15 11:41:38 +0400

    merged settings branch

--- app/src/main/java/ge/tbca/city_park/di/RepositoryModule.kt
+++ app/src/main/java/ge/tbca/city_park/di/RepositoryModule.kt
@@ -7,11 +7,11 @@ import dagger.hilt.components.SingletonComponent
 import ge.tbca.city_park.data.auth.repository.AuthRepositoryImpl
 import ge.tbca.city_park.data.auth.repository.PasswordRepositoryImpl
 import ge.tbca.city_park.data.auth.repository.SignUpRepositoryImpl
+import ge.tbca.city_park.data.repository.DataStoreManagerImpl
+import ge.tbca.city_park.data.repository.ThemePreferenceRepositoryImpl
 import ge.tbca.city_park.domain.features.auth.repository.AuthRepository
 import ge.tbca.city_park.domain.features.auth.repository.PasswordRepository
 import ge.tbca.city_park.domain.features.auth.repository.SignUpRepository
-import ge.tbca.city_park.data.repository.DataStoreManagerImpl
-import ge.tbca.city_park.data.repository.ThemePreferenceRepositoryImpl
 import ge.tbca.city_park.domain.repository.DataStoreManager
 import ge.tbca.city_park.domain.repository.ThemePreferenceRepository
 import javax.inject.Singleton
@@ -25,10 +25,10 @@ abstract class RepositoryModule {
     abstract fun bindsAuthRepository(impl: AuthRepositoryImpl): AuthRepository
 
     @Singleton
-    abstract fun bindDatastoreManager(
-        dataStoreManagerImpl: DataStoreManagerImpl
-    ): DataStoreManager
+    @Binds
+    abstract fun bindDatastoreManager(dataStoreManagerImpl: DataStoreManagerImpl): DataStoreManager
 
+    @Singleton
     @Binds
     abstract fun bindSignUpRepository(impl: SignUpRepositoryImpl): SignUpRepository
 
@@ -36,7 +36,7 @@ abstract class RepositoryModule {
     @Binds
     abstract fun bindsPasswordRepository(impl: PasswordRepositoryImpl): PasswordRepository
 
-    abstract fun bindThemePreferenceRepository(
-        themePreferenceRepositoryImpl: ThemePreferenceRepositoryImpl
-    ): ThemePreferenceRepository
+    @Singleton
+    @Binds
+    abstract fun bindThemePreferenceRepository(imp: ThemePreferenceRepositoryImpl): ThemePreferenceRepository
 }
