fdf711b640887dd40c82c2510571f5e360ab06ad
davidbera1 <dataaberadze@gmail.com>
2025-04-24 21:17:18 +0400

    added AddCarNavGraph

---
+++ feature/cars/presentation/src/main/java/ge/tbca/city_park/cars/presentation/navigation/AddCarNavGraph.kt
@@ -0,0 +1,21 @@
+package ge.tbca.city_park.cars.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import ge.tbca.city_park.cars.presentation.screen.add_car.AddCarScreenRoot
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.addCarNavGraph(
+    navigateBack: () -> Unit,
+    onShowSnackBar: (String) -> Unit,
+) {
+    composable<AddCarScreenRoute> {
+        AddCarScreenRoot(
+            onShowSnackBar = onShowSnackBar,
+            navigateBack = navigateBack
+        )
+    }
+}
+
+@Serializable
+data object AddCarScreenRoute
