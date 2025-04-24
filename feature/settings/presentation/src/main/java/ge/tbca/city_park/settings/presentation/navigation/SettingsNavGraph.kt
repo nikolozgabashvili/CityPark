314ff4958b8fa34522517a7e6fb8090f7a2b9328
davidbera1 <dataaberadze@gmail.com>
2025-04-24 21:02:48 +0400

    added SettingsNavGraph

---
+++ feature/settings/presentation/src/main/java/ge/tbca/city_park/settings/presentation/navigation/SettingsNavGraph.kt
@@ -0,0 +1,43 @@
+package ge.tbca.city_park.settings.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import ge.tbca.city_park.settings.presentation.language_settings.screen.LanguageSettingsScreenRoot
+import ge.tbca.city_park.settings.presentation.settings.screen.SettingsScreenRoot
+import ge.tbca.city_park.settings.presentation.theme_settings.screen.ThemeSettingsScreenRoot
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.settingsNavGraph(
+    navigateBack: () -> Unit,
+    navigateToLanguageSettings: () -> Unit,
+    navigateToThemeSettings: () -> Unit
+) {
+    composable<SettingsScreenRoute> {
+        SettingsScreenRoot(
+            navigateBack = navigateBack,
+            navigateToTheme = navigateToThemeSettings,
+            navigateToLanguage = navigateToLanguageSettings
+        )
+    }
+
+    composable<LanguageSettingsScreenRoute> {
+        LanguageSettingsScreenRoot(
+            navigateBack = navigateBack
+        )
+    }
+
+    composable<ThemeSettingsScreenRoute> {
+        ThemeSettingsScreenRoot(
+            navigateBack = navigateBack
+        )
+    }
+}
+
+@Serializable
+data object SettingsScreenRoute
+
+@Serializable
+data object LanguageSettingsScreenRoute
+
+@Serializable
+data object ThemeSettingsScreenRoute
